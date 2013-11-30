727fc928b163c7efbf0faec2a5170ad0f5f7bb44
dmsilaev <dmsilaev@yandex.ru>
2013-11-30 22:35:10 +0400

    delete unused files

--- test/decorators/api/admin/stories_decorator_test.rb
+++
@@ -1,5 +0,0 @@
-# -*- encoding : utf-8 -*-
-require 'test_helper'
-
-class Api::Admin::StoriesDecoratorTest < Draper::TestCase
-end
--- test/helpers/api/admin/stories_helper_test.rb
+++
@@ -1,5 +0,0 @@
-# -*- encoding : utf-8 -*-
-require 'test_helper'
-
-class Api::Admin::StoriesHelperTest < ActionView::TestCase
-end
