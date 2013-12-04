57fb691b92f3958c296627b863de3b95756e6ea7
dmsilaev <dmsilaev@yandex.ru>
2013-12-05 02:38:07 +0400

    heroku settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Weblog::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.serve_static_assets = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
